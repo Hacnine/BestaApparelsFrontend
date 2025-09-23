5505135ee44b92d4defa72220e81a848232d96a6
Hasanain <hasanaintushar@gmail.com>
2025-09-23 15:21:05 -0700

    Delete AdminRoutes.tsx

--- src/routes/AdminRoutes.tsx
+++
@@ -1,9 +0,0 @@
-import { useUser } from "@/redux/slices/userSlice";
-import { Navigate, Outlet } from "react-router-dom";
-
-const AdminRoutes = () => {
-  const {user} = useUser();
-  return user.role === 'ADMIN' ? <Outlet /> : <Navigate to="/signin" />;
-};
-
-export default AdminRoutes;
